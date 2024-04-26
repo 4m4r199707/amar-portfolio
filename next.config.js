b700e0e631e069cd8ecdbce9b2ec1abc9880267b
4m4r199707 <4m4rarts@gmail.com>
2024-04-26 17:42:28 +0530

    image build fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '4m4r199707.github.io',
-          port: '',
-        },
-      ],
-    },
+    output: "export",
   }
